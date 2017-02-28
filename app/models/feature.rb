d2617be75e83a6a79b89573b91ee13f805902696
Malick Diarra <malick.diarra@excella.com>
2017-02-28 14:24:49 +0000

    user

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -8,4 +8,7 @@ class Feature < ActiveRecord::Base
 
   feature :placeholder_over_label,
           description: 'When this toggle is turned on, there will no longer be labels.'
+
+  feature :edit_in_place,
+          description: 'When this feature toggle is on a user will now be able to update and show instead of refreshing the page'
 end
