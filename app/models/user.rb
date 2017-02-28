a0ac6883d16c7fa167782c0e870f11853428ad9a
Phil <phil.boothe@gmail.com>
2017-02-28 09:24:31 -0500

    refactor toggle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :first_name, presence: true
-  # validates :middle_name, presence: true
+  validates :middle_name, presence: true
   validates :last_name, presence: true
   validates :role, presence: true
 
